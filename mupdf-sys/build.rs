be68b7c8483814f124a4b5025691d0b37ca06744
Yves Zoundi <yveszoundi@gmail.com>
2025-05-07 10:06:33 -0400

    Fix typo

--- mupdf-sys/build.rs
+++ mupdf-sys/build.rs
@@ -527,7 +527,7 @@ fn main() {
     println!("cargo:rerun-if-changed=wrapper.c");
 
     if let Ok(ref target_os) = env::var("CARGO_CFG_TARGET_OS") {
-        if target_os == "macos" || target_os = "windows" {
+        if target_os == "macos" || target_os == "windows" {
             println!("cargo:rustc-link-lib=c++");
         } else {
             println!("cargo:rustc-link-lib=stdc++");
