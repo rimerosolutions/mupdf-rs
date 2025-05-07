818229bfa84db128911b98e91f681b791acfd5b3
Yves Zoundi <yveszoundi@gmail.com>
2025-05-07 11:37:30 -0400

    Adjust flags

--- mupdf-sys/build.rs
+++ mupdf-sys/build.rs
@@ -527,12 +527,16 @@ fn main() {
     println!("cargo:rerun-if-changed=wrapper.c");
 
     if let Ok(ref target_os) = env::var("CARGO_CFG_TARGET_OS") {
-        if target_os != "windows" {
-            if target_os == "macos" {
-                println!("cargo:rustc-link-lib=c++");
-            } else {
-                println!("cargo:rustc-link-lib=stdc++");
-            }
+        if target_os == "windows" {
+            #[cfg(target_env = "msvc")]
+            println!("cargo:rustc-link-lib=msvcrt");
+
+            #[cfg(not(target_env = "msvc"))]
+            println!("cargo:rustc-link-lib=stdc++");
+        } else if target_os == "macos" {
+            println!("cargo:rustc-link-lib=c++");
+        } else {
+            println!("cargo:rustc-link-lib=stdc++");
         }
     }
 
